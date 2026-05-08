aed0848819f8d113ed5d4d36e28db5fe874eae40
Yassine-ala <yassine.ala@yahoo.com>
2026-05-08 20:38:58 +0200

    Implement categories loading and state management

--- src/app/features/categories/pages/category-selector-page/category-selector-page.ts
+++ src/app/features/categories/pages/category-selector-page/category-selector-page.ts
@@ -1,49 +1,48 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { finalize, forkJoin } from 'rxjs';
+
 import { CategoriesApiService } from '../../api/categories-api.service';
 import { Category } from '../../models/category.model';
-import { forkJoin } from 'rxjs';
 import { CategoryList } from '../../components/category-list/category-list';
 
 @Component({
   selector: 'app-category-selector-page',
-  imports: [
-    CategoryList
-  ],
+  imports: [CategoryList],
   templateUrl: './category-selector-page.html',
   styleUrl: './category-selector-page.scss',
 })
 export class CategorySelectorPage implements OnInit {
   private readonly _categoriesApi = inject(CategoriesApiService);
-  isLoading = false;
-  hasError = false;
 
-  categories: Category[] = [];
+  readonly isLoading = signal(false);
+  readonly hasError = signal(false);
+  readonly categories = signal<Category[]>([]);
 
   ngOnInit(): void {
     this._loadCategories();
   }
 
-  // requêtes indépendantes -> forkJoin pour parallèliser
+  // requêtes indépendantes -> forkJoin pour paralléliser
   private _loadCategories(): void {
-    this.isLoading = true;
-    this.hasError = false;
+    this.isLoading.set(true);
+    this.hasError.set(false);
 
     forkJoin([
       this._categoriesApi.getAllCategories(),
       this._categoriesApi.getVisibleCategoryIds(),
-    ]).subscribe({
-      next: ([categories, visibleIds]) => {
-        this.categories = categories.filter(category =>
-          visibleIds.includes(category.id)
-        );
-      },
-      error: () => {
-        this.hasError = true;
-        this.isLoading = false;
-      },
-      complete: () => {
-        this.isLoading = false;
-      },
-    });
+    ])
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: ([categories, visibleIds]) => {
+          this.categories.set(
+            categories.filter(category => visibleIds.includes(category.id))
+          );
+        },
+        error: error => {
+          console.error(error);
+          this.hasError.set(true);
+        },
+      });
   }
 }
