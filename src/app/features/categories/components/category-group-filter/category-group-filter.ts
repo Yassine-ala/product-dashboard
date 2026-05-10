f81e4de30a9a7c6a94923c980a30f2bb5de1dcfc
Yassine-ala <yassine.ala@yahoo.com>
2026-05-10 21:17:22 +0200

    Merge search and filter into toolbar component

--- src/app/features/categories/components/category-group-filter/category-group-filter.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-category-group-filter',
-  imports: [],
-  templateUrl: './category-group-filter.html',
-  styleUrl: './category-group-filter.scss',
-})
-export class CategoryGroupFilter {}
--- src/app/features/categories/components/category-search/category-search.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-category-search',
-  imports: [],
-  templateUrl: './category-search.html',
-  styleUrl: './category-search.scss',
-})
-export class CategorySearch {}
